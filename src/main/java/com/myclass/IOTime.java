f1813d6932ed00c7a298161031ea137f90325542
shot-mk <shot-mk@yandex.ru>
2013-10-28 19:35:25 +0200

    First commit

---
+++ src/main/java/com/myclass/IOTime.java
@@ -0,0 +1,12 @@
+package com.myclass;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikita
+ * Date: 28.10.13
+ * Time: 19:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class IOTime {
+
+}
